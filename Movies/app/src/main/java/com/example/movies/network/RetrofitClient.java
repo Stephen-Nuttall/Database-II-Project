e750ee152cf389405cf87dc30885d4835ffc0b8e
Ilyes Bouazza <73422420+ilyes256@users.noreply.github.com>
2025-04-20 04:53:40 -0400

    feat: add Retrofit client configuration for API communication

--- Movies/app/src/main/java/com/example/movies/network/RetrofitClient.java
+++ Movies/app/src/main/java/com/example/movies/network/RetrofitClient.java
@@ -1,4 +1,22 @@
 package com.example.movies.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+
+    //Replace it with your ip and port , so it can work
+    private static final String BASE_URL = "http://10.0.2.2:1234/Database-II-Project/phase%203/club_api/";
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create()) // for JSON parsing
+                    .build();
+        }
+        return retrofit;
+    }
 }
